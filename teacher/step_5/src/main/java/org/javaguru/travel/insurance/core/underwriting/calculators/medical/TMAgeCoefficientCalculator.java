d01c31f0e9e39fb03cf5651644c27ff330faf0cd
javagurulv <javagurulv@gmail.com>
2024-02-20 08:41:13 +0200

    step_5

---
+++ teacher/step_5/src/main/java/org/javaguru/travel/insurance/core/underwriting/calculators/medical/TMAgeCoefficientCalculator.java
@@ -0,0 +1,55 @@
+package org.javaguru.travel.insurance.core.underwriting.calculators.medical;
+
+import org.javaguru.travel.insurance.core.api.dto.PersonDTO;
+import org.javaguru.travel.insurance.core.domain.TMAgeCoefficient;
+import org.javaguru.travel.insurance.core.repositories.TMAgeCoefficientRepository;
+import org.javaguru.travel.insurance.core.util.DateTimeUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Component
+class TMAgeCoefficientCalculator {
+
+    @Value( "${medical.risk.age.coefficient.enabled:false}" )
+    private Boolean medicalRiskAgeCoefficientEnabled;
+
+    @Autowired private DateTimeUtil dateTimeUtil;
+    @Autowired private TMAgeCoefficientRepository ageCoefficientRepository;
+
+    BigDecimal calculate(PersonDTO person) {
+        return medicalRiskAgeCoefficientEnabled
+                ? getCoefficient(person)
+                : getDefaultValue();
+    }
+
+    private BigDecimal getCoefficient(PersonDTO person) {
+        int age = calculateAge(person);
+        return ageCoefficientRepository.findCoefficient(age)
+                .map(TMAgeCoefficient::getCoefficient)
+                .orElseThrow(() -> new RuntimeException("Age coefficient not found for age = " + age));
+    }
+
+    private Integer calculateAge(PersonDTO person) {
+        LocalDate personBirthDate = toLocalDate(person.getPersonBirthDate());
+        LocalDate currentDate = toLocalDate(dateTimeUtil.getCurrentDateTime());
+        return Period.between(personBirthDate, currentDate).getYears();
+    }
+
+    private LocalDate toLocalDate(Date date) {
+        return date.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+    }
+
+    private static BigDecimal getDefaultValue() {
+        return BigDecimal.ONE;
+    }
+
+}
