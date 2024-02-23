d12314de1f809f8f07e92b172da7921377f6f67f
javagurulv <javagurulv@gmail.com>
2024-02-23 16:18:09 +0200

    step_17 - RabbitMQ message broker setup

---
+++ teacher/step_17/src/main/java/org/javaguru/travel/insurance/dto/v1/TravelCalculatePremiumResponseV1.java
@@ -0,0 +1,55 @@
+package org.javaguru.travel.insurance.dto.v1;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.javaguru.travel.insurance.dto.CoreResponse;
+import org.javaguru.travel.insurance.dto.RiskPremium;
+import org.javaguru.travel.insurance.dto.ValidationError;
+import org.javaguru.travel.insurance.dto.util.BigDecimalSerializer;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class TravelCalculatePremiumResponseV1 extends CoreResponse {
+
+    private String uuid;
+    private String personFirstName;
+    private String personLastName;
+    private String personCode;
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private Date personBirthDate;
+
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private Date agreementDateFrom;
+
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private Date agreementDateTo;
+
+    private String country;
+
+    private String medicalRiskLimitLevel;
+
+    @JsonSerialize(using = BigDecimalSerializer.class)
+    private BigDecimal travelCost;
+
+    @JsonSerialize(using = BigDecimalSerializer.class)
+    private BigDecimal agreementPremium;
+
+    private List<RiskPremium> risks;
+
+    public TravelCalculatePremiumResponseV1(List<ValidationError> errors) {
+        super(errors);
+    }
+
+}
