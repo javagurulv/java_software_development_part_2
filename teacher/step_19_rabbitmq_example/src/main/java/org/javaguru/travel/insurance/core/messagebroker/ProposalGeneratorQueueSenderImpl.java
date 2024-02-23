f6a306ba4c809820148b8e4970d654142a62d9ae
javagurulv <javagurulv@gmail.com>
2024-02-23 19:20:12 +0200

    rabbitmq connection example

---
+++ teacher/step_19_rabbitmq_example/src/main/java/org/javaguru/travel/insurance/core/messagebroker/ProposalGeneratorQueueSenderImpl.java
@@ -0,0 +1,34 @@
+package org.javaguru.travel.insurance.core.messagebroker;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.javaguru.travel.insurance.core.api.dto.AgreementDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+class ProposalGeneratorQueueSenderImpl implements ProposalGeneratorQueueSender {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProposalGeneratorQueueSenderImpl.class);
+
+    @Autowired private RabbitTemplate rabbitTemplate;
+
+    @Override
+    public void send(AgreementDTO agreement) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String json = objectMapper.writeValueAsString(agreement);
+            logger.info("PROPOSAL GENERATION message content: " + json);
+            rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUE_PROPOSAL_GENERATION, json);
+        } catch (JsonProcessingException e) {
+            logger.error("Error to convert agreement to JSON", e);
+        } catch (AmqpException e) {
+            logger.error("Error to sent proposal generation message", e);
+        }
+    }
+
+}
