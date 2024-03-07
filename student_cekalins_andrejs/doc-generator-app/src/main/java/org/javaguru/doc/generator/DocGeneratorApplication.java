1c273f0030349e9ffee90f4f931489182c2a43f6
AndrejsC <batarejka2006@gmail.com>
2024-03-07 13:47:19 +0200

    software_development_part_2_student_cekalins_andrejs_step_22_DocGenerator_app

---
+++ student_cekalins_andrejs/doc-generator-app/src/main/java/org/javaguru/doc/generator/DocGeneratorApplication.java
@@ -0,0 +1,18 @@
+package org.javaguru.doc.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableCaching
+@EnableScheduling
+@SpringBootApplication
+public class DocGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocGeneratorApplication.class, args);
+    }
+
+}
+
