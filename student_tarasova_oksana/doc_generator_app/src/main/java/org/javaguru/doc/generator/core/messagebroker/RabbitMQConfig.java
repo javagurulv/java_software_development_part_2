438ade31978fc1f4600e41cc98284cf96803758b
Оксана Tarasova <oksana_sineok@mail.ru>
2024-03-07 14:56:42 +0200

    task_27

--- student_tarasova_oksana/doc_generator_app/src/main/java/org/javaguru/doc/generator/core/messagebroker/RabbitMQConfig.java
+++ student_tarasova_oksana/doc_generator_app/src/main/java/org/javaguru/doc/generator/core/messagebroker/RabbitMQConfig.java
@@ -10,10 +10,16 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
     public static final String QUEUE_PROPOSAL_GENERATION = "q.proposal-generation";
+    public static final String QUEUE_PROPOSAL_GENERATION_DLQ = "q.proposal-generation-dlq";
 
     @Bean
     public Queue createProposalPdfGenerationQueue() {
         return new Queue(QUEUE_PROPOSAL_GENERATION);
+    }
+
+    @Bean
+    public Queue proposalPdfGenerationDeadLetterQueue() {
+        return new Queue(QUEUE_PROPOSAL_GENERATION_DLQ);
     }
 
 }
