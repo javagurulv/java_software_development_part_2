ff781bfd212dc5b974644950002b9450ccc75b76
javagurulv <javagurulv@gmail.com>
2024-03-10 15:10:25 +0200

    step_34 - BlackList core business logic

---
+++ teacher/step_34/insurance-calculator-app/src/main/java/org/javaguru/travel/insurance/core/messagebroker/proposal/ProposalGeneratorQueueSender.java
@@ -0,0 +1,9 @@
+package org.javaguru.travel.insurance.core.messagebroker.proposal;
+
+import org.javaguru.travel.insurance.core.api.dto.AgreementDTO;
+
+public interface ProposalGeneratorQueueSender {
+
+    void send(AgreementDTO agreement);
+
+}
