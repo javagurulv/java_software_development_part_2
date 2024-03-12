7e1245e31b059c138f38b4916ca23701691eb15a
AndrejsC <batarejka2006@gmail.com>
2024-03-12 18:33:09 +0200

    software_development_part_2_student_cekalins_andrejs_Dockerfile_and_wait_for_it_refactoring

--- student_cekalins_andrejs/doc-generator-app/src/main/java/org/javaguru/doc/generator/DocGeneratorApplication.java
+++ student_cekalins_andrejs/doc-generator-app/src/main/java/org/javaguru/doc/generator/DocGeneratorApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableCaching
-@EnableScheduling
+
 @SpringBootApplication
 public class DocGeneratorApplication {
 
