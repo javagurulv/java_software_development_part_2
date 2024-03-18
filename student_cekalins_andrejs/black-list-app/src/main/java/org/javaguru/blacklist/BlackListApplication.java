0163a66b168b251d1b7ca1fbfa7fb6a8af878695
AndrejsCekalins <batarejka2006@gmail.com>
2024-03-18 19:42:02 +0200

    software_development_part_2_student_cekalins_andrejs_step_33_black-list-app_DAL_creation

---
+++ student_cekalins_andrejs/black-list-app/src/main/java/org/javaguru/blacklist/BlackListApplication.java
@@ -0,0 +1,12 @@
+package org.javaguru.blacklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackListApplication.class, args);
+    }
+}
