afbb320cb836d593c96279a3432cfc3e4aec867d
AndrejsCekalins <batarejka2006@gmail.com>
2024-03-26 18:35:30 +0200

    software_development_part_2_student_cekalins_andrejs_step_37_blackList_REST_API_synchronization

---
+++ student_cekalins_andrejs/insurance_calculator_v2/src/main/java/org/javaguru/travel/insurance/core/blacklist/BlackListPersonCheckService.java
@@ -0,0 +1,8 @@
+package org.javaguru.travel.insurance.core.blacklist;
+
+import org.javaguru.travel.insurance.core.api.dto.PersonDTO;
+
+public interface BlackListPersonCheckService {
+
+    boolean isPersonBlacklisted (PersonDTO personDTO);
+}
---
+++ student_cekalins_andrejs/insurance_calculator_v2/src/main/java/org/javaguru/travel/insurance/core/blacklist/dto/BlackListedPersonCheckResponse.java
@@ -0,0 +1,28 @@
+package org.javaguru.travel.insurance.core.blacklist.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.javaguru.travel.insurance.dto.CoreResponse;
+import org.javaguru.travel.insurance.dto.ValidationError;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class BlackListedPersonCheckResponse extends CoreResponse {
+
+    private String personFirstName;
+    private String personLastName;
+    private String personCode;
+    private Boolean blacklisted;
+
+    public BlackListedPersonCheckResponse(List<ValidationError> errors) {
+        super(errors);
+    }
+}
