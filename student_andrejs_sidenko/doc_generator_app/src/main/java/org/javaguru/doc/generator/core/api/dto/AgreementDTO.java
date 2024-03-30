a7819b5bd05f10e6da99265da70567600fadcdbb
Anetobiz <anetobiz@gmail.com>
2024-03-30 17:45:07 +0200

    doc generator DTO file creation

---
+++ student_andrejs_sidenko/doc_generator_app/src/main/java/org/javaguru/doc/generator/core/api/dto/AgreementDTO.java
@@ -0,0 +1,28 @@
+package org.javaguru.doc.generator.core.api.dto;
+
+import lombok.*;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AgreementDTO {
+
+    private String uuid;
+    private Date agreementDateFrom;
+
+    private Date agreementDateTo;
+
+    private String country;
+
+    private List<String> selectedRisks;
+
+    private List<PersonDTO> persons;
+
+    private BigDecimal agreementPremium;
+
+}
