d2d9a30e83076def3052fea3021c71d0110a23da
Anetobiz <anetobiz@gmail.com>
2024-04-06 12:24:13 +0300

    dead-letter-queue added.

--- student_andrejs_sidenko/doc_generator_app/src/main/java/org/javaguru/doc/generator/core/messagebroker/ProposalGenerationQueueListener.java
+++ student_andrejs_sidenko/doc_generator_app/src/main/java/org/javaguru/doc/generator/core/messagebroker/ProposalGenerationQueueListener.java
@@ -3,7 +3,9 @@ package org.javaguru.doc.generator.core.messagebroker;
 import org.javaguru.doc.generator.core.api.dto.AgreementDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -19,20 +21,47 @@ public class ProposalGenerationQueueListener {
 
     private static final Logger logger = LoggerFactory.getLogger(ProposalGenerationQueueListener.class);
 
-    @Autowired
-    private JsonStringToAgreementDtoConverter agreementDtoConverter;
+    @Value("${rabbitmq.total.retry.count:3}")
+    private Integer totalRetryCount;
+
+    @Autowired private JsonStringToAgreementDtoConverter agreementDtoConverter;
     @Autowired private ProposalGenerator proposalGenerator;
+    @Autowired private RabbitTemplate rabbitTemplate;
 
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE_PROPOSAL_GENERATION)
-    public void receiveMessage(String message) throws IOException {
+    public void receiveMessage(final Message message) throws Exception {
         try {
-            logger.info(message);
-            AgreementDTO agreementDTO = agreementDtoConverter.convert(message);
-            proposalGenerator.generateProposalAndStoreToFile(agreementDTO);
+            processMessage(message);
         } catch (Exception e) {
             logger.error("FAIL to process message: ", e);
+            retryOrForwardToDeadLetterQueue(message);
         }
     }
+
+    private void retryOrForwardToDeadLetterQueue(Message message) {
+        Integer retryCount = message.getMessageProperties().getHeader("x-retry-count");
+        logger.info("MESSAGE DELIVERY TAG "
+                + message.getMessageProperties().getDeliveryTag()
+                + " RETRY COUNT = " + retryCount);
+        if (retryCount == null) {
+            retryCount = 0;
+        }
+        retryCount++;
+        if (retryCount <= totalRetryCount) {
+            message.getMessageProperties().setHeader("x-retry-count", retryCount);
+            rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUE_PROPOSAL_GENERATION, message);
+        } else {
+            rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUE_PROPOSAL_GENERATION_DLQ, message);
+        }
+    }
+
+    private void processMessage(Message message) throws Exception {
+        String messageBody = new String(message.getBody());
+        logger.info(messageBody);
+        AgreementDTO agreementDTO = agreementDtoConverter.convert(messageBody);
+        proposalGenerator.generateProposalAndStoreToFile(agreementDTO);
+    }
+
 
 }
